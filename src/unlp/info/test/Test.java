771f540f57f1144e1f5dcea46d0feae2ea925b5e
lbudnik <lucasbudnik@hotmail.com.ar>
2023-12-10 00:34:38 -0300

    se planteo una solucion, y otra solucion posible en forma de comentario

--- src/unlp/info/test/Test.java
+++ src/unlp/info/test/Test.java
@@ -1,20 +1,27 @@
 package unlp.info.test;
 
-import unlp.info.animal.*;
-import unlp.info.animalespeligrosos.*;
-import unlp.info.domestico.*;
+import unlp.info.animal.PerroPeligroso;
+//import unlp.info.animalespeligrosos.AnimalPeligroso;
+//import unlp.info.domestico.PerroDomestico;
 
 public class Test {
-
+	/*
+	 * Establecemos una solucion, en la cual el perro directamente es del tipo
+	 * PerroPeligroso, el cual implementa todo. El otro metodo seria eliminando las
+	 * funcionalidades que el perro no tiene permitidas en cada caso: en el caso del
+	 * perro gordo eliminar que muerda y en el caso de rita, eliminar que ladre y
+	 * que mueva la cola
+	 */
 	public static void main(String[] args) {
-		PerroDomestico gordo = new PerroPeligroso();
+		PerroPeligroso gordo = new PerroPeligroso();
 		gordo.ladra();
 		gordo.mueveCola();
 		gordo.muerde();
-		AnimalPeligroso rita = new PerroPeligroso();
+		PerroPeligroso rita = new PerroPeligroso();
 		rita.muerde();
 		rita.ladra();
 		rita.mueveCola();
+
 	}
 
 }
