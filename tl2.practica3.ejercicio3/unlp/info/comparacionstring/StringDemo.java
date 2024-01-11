7233fafc5c8315bcd3b116dc4e001fe13b4f9c3e
lbudnik <lucasbudnik@hotmail.com.ar>
2024-01-11 11:15:59 -0300

    Actualizacion Practica 3 ejercicios 3

--- tl2.practica3.ejercicio3/unlp/info/comparacionstring/StringDemo.java
+++ tl2.practica3.ejercicio3/unlp/info/comparacionstring/StringDemo.java
@@ -2,11 +2,11 @@ package unlp.info.comparacionstring;
 
 public class StringDemo {
 	public static void main(String[] args) {
-		String str1 = "Leones y Tigres y Osos!"; //Lit
-		String str2 = "Leones y Tigres y Osos!"; //Lit
+		String str1 = "Leones y Tigres y Osos!"; // Lit
+		String str2 = "Leones y Tigres y Osos!"; // Lit
 		String str3 = str2; // Lit
 		String str4 = new String("Leones y Tigres y Osos!"); // OBJ
-		String str5 = " Y yo!"; //LIT
+		String str5 = " Y yo!"; // LIT
 		String str6 = "Leones y Tigres y Osos! Y yo!"; // lit
 		String str7 = str1 + str5; // lit
 		/*----------------------------------*/
@@ -14,7 +14,7 @@ public class StringDemo {
 		System.out.println(str1 == str3); // true Comparacion de dos literales
 		System.out.println(str1 == str4); // false Comparacion Literal y Object
 		System.out.println(str2 == str3); // true Comparacion Dos literales igualados
-		
+
 		System.out.println(str2 == str4); // false Comparacion Literal y Object
 		System.out.println(str3 == str4); // false Comparacion Literal y Object
 		System.out.println(str6 == str7); // unico error. Es False
@@ -23,7 +23,8 @@ public class StringDemo {
 		/*----------------------------------*/
 		System.out.println(str1.equals(str4)); // true mediante equals podemos comparar
 		// tanto String Object con String Object como String Object con Literal String
-		// siempre y cuando no se trate de un objeto con implementacion propia del equals
+		// siempre y cuando no se trate de un objeto con implementacion propia del
+		// equals
 		// lo cual depende del criterio de programador el resultado
 		System.out.println(str6.equals(str7)); // true misma justificacion del anterior.
 	}
